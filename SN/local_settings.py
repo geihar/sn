ea0fd814a77509c785607e3b06ae7172a99bec3e
geihar <l0635968488@gmail.com>
2020-04-09 17:48:39 +0200

    add new app

---
+++ SN/local_settings.py
@@ -0,0 +1,17 @@
+EMAIL_HOST_USER ='supppor718@gmail.com'
+EMAIL_HOST_PASSWORD ='ghjuekrf33'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
+    }
+}
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '216880912269-jkp3mf1itqrrhjskj46f8pdfckdl5nlg.apps.googleusercontent.com' # Google Consumer Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'cWupQ9gkR9KVqXPTJtoN1rg8' # Google Consumer Secret
+SOCIAL_AUTH_FACEBOOK_KEY = '253265545849675' # Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = 'ef9cdc10bf50221a57cb7cd6c5b754d7' # Facebook App Secret
